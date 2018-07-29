44fd71c43a31ca6e597cd38a4bc43ba82adff38b
ChangJoo Park <pcjpcj2@gmail.com>
2018-07-29 17:15:02 +0900

    Update user model for project and assigned Issues

--- models/User.js
+++ models/User.js
@@ -3,7 +3,10 @@ const crypto = require('crypto')
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
+  email: {
+    type: String,
+    unique: true
+  },
   password: String,
   passwordResetToken: String,
   passwordResetExpires: Date,
@@ -23,19 +26,35 @@ const userSchema = new mongoose.Schema({
     location: String,
     website: String,
     picture: String
-  }
-}, { timestamps: true })
+  },
+  projects: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Project'
+  }],
+  assignedIssues: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Project'
+  }]
+}, {
+  timestamps: true
+})
 
 /**
  * Password hash middleware.
  */
 userSchema.pre('save', function save (next) {
   const user = this
-  if (!user.isModified('password')) { return next() }
+  if (!user.isModified('password')) {
+    return next()
+  }
   bcrypt.genSalt(10, (err, salt) => {
-    if (err) { return next(err) }
+    if (err) {
+      return next(err)
+    }
     bcrypt.hash(user.password, salt, null, (err, hash) => {
-      if (err) { return next(err) }
+      if (err) {
+        return next(err)
+      }
       user.password = hash
       next()
     })
