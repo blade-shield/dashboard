0e95c22daae83295a2b34ce05eb0408de53d29e1
ChangJoo Park <pcjpcj2@gmail.com>
2018-07-29 21:59:10 +0900

    Update CRUD

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,11 +1,12 @@
 const Project = require('../models/Project')
+const User = require('../models/User')
 const projectTypes = require('../config/project-types')
+const wrap = require('express-async-wrap')
+
 /**
  * GET /projects
  */
 exports.getProjects = (req, res) => {
-  console.log(req.user)
-
   Project.find({
     users: req.user._id
   })
@@ -21,11 +22,21 @@ exports.getProjects = (req, res) => {
 /**
  * GET /projects/:id
  */
-exports.getProjectById = (req, res) => {
-  res.render('projects/show', {
-    title: 'Project'
-  })
-}
+exports.getProjectById = wrap(async (req, res) => {
+  try {
+    const project = await Project.findById(req.params['id'])
+
+    res.render('projects/show', {
+      title: 'Project',
+      project
+    })
+  } catch (error) {
+    req.flash('errors', {
+      msg: 'Project not found'
+    })
+    res.redirect('/projects')
+  }
+})
 
 /**
  * GET /projects/new
@@ -40,34 +51,89 @@ exports.newProject = (req, res) => {
 /**
  * POST /projects/new
  */
-exports.createProject = (req, res) => {
-  console.log('NEW PROJECT')
+exports.createProject = wrap(async (req, res) => {
+  const {
+    _id: userId
+  } = req.user
+  const {
+    name,
+    type
+  } = req.body
 
-  console.log(req.body)
+  const newProject = new Project()
+  newProject.name = name
+  newProject.type = type
+  newProject.owner = userId
+  newProject.users.push(userId)
+
+  const savedProject = await newProject.save()
+
+  await User.update({
+    _id: userId
+  }, {
+    $push: {
+      projects: savedProject._id
+    }
+  })
+
   req.flash('success', {
     msg: 'Project has been created successfully!'
   })
   res.redirect('/projects')
-}
+})
 
 /**
  * GET /projects/:id/edit
  */
-exports.editProject = (req, res) => {
-  res.render('projects/:id/edit', {
-    title: 'Project: Edit'
-  })
-}
-
+exports.editProject = wrap(async (req, res) => {
+  try {
+    const project = await Project.findById(req.params['id']).populate('owner').populate('users')
+    res.render('projects/edit', {
+      title: 'Project: Edit',
+      types: projectTypes,
+      project
+    })
+  } catch (error) {
+    req.flash('errors', {
+      msg: 'Project not found'
+    })
+    res.redirect('/projects')
+  }
+})
 /**
- * PUT /projects/:id/edit
+ * PUT /projects/:id
  */
-exports.updateProject = (req, res) => {
-  console.log('NEW PROJECT')
+exports.updateProject = wrap(async (req, res) => {
+  try {
+    const project = await Project.findById(req.params['id'])
+    const {
+      name,
+      type,
+      slack
+    } = req.body
+    project.name = name
+    project.type = type
+    // Parse Slack
+    const parsedSlack = {
+      isEnabled: slack.isEnabled === 'on',
+      url: slack.url || '',
+      webhook: slack.webhook || '',
+      channel: slack.channel || ''
+    }
 
-  console.log(req.body)
-  req.flash('success', {
-    msg: 'Project has been created successfully!'
-  })
-  res.redirect('/projects')
-}
+    project.slack = parsedSlack
+
+    await project.save()
+
+    req.flash('success', {
+      msg: 'Project has been updated successfully!'
+    })
+    res.redirect('/projects')
+  } catch (error) {
+    console.log(error)
+    req.flash('errors', {
+      msg: 'Project not found'
+    })
+    res.redirect('/projects')
+  }
+})
