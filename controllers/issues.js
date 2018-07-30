8994cde475b5854c197d607d23bfbc96e8bb6a1f
ChangJoo Park <pcjpcj2@gmail.com>
2018-07-30 18:57:36 +0900

    Add createIssue route

--- controllers/issues.js
+++ controllers/issues.js
@@ -1,5 +1,5 @@
 const Project = require('../models/Project')
-const User = require('../models/User')
+const Issue = require('../models/Issue')
 const wrap = require('express-async-wrap')
 
 exports.getIssueById = wrap(async (req, res) => {
@@ -11,14 +11,42 @@ exports.getIssueById = wrap(async (req, res) => {
 })
 
 exports.newIssue = wrap(async (req, res) => {
-  const project = await Project.findById(req.params['projectId'])
-
+  const {
+    project
+  } = res.locals
   res.render('projects/issues/new', {
     project
   })
 })
 
 exports.createIssue = wrap(async (req, res) => {
-  console.log('createIssue')
+  const {
+    project
+  } = res.locals
+  console.log('res.locals => ', project)
+  const {
+    title,
+    url,
+    message,
+    isResolved = false
+  } = req.body
+
+  const newIssue = await new Issue()
+  newIssue.title = title
+  newIssue.url = url
+  newIssue.isResolved = isResolved
+  newIssue.message = message
+  newIssue.project = project._id
+
+  const savedIssue = await newIssue.save()
+  console.log('savedIssue => ', savedIssue)
+  await Project.update({
+    _id: project._id
+  }, {
+    $push: {
+      issues: savedIssue._id
+    }
+  })
+
   res.redirect(`/projects/${req.params['projectId']}`)
 })
